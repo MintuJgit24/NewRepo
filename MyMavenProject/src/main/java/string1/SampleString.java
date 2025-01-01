bedf309e33073571707b7519cc109aa3f6cbf845
mintu <mintu@VJ-GALAXY-BOOK>
2025-01-01 11:26:13 +0530

    committing first project

---
+++ MyMavenProject/src/main/java/string1/SampleString.java
@@ -0,0 +1,40 @@
+package string1;
+
+public class SampleString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringBuffer sb=new StringBuffer("hello");
+		System.out.println(sb);
+		
+		StringBuilder sbs=new StringBuilder("world");
+		System.out.println(sbs);
+		
+		StringBuilder sb1=new StringBuilder("nice");
+		
+		
+		//append-to add string at end
+		sb.append(" mintu");
+		sbs.append(" Java");
+		System.out.println(sb);
+		System.out.println(sbs);
+		
+		//insert-to add at particular index
+		sb.insert(3,"hey");
+		System.out.println(sb);
+		
+		//replace-it will add string in the 1st index and takes 3 places of the index starting at 0
+		sb1.replace(1, 3, "happy");
+		System.out.println(sb1);
+		
+		//reverse-to reverse a string
+		sb1.reverse();
+		System.out.println(sb1);
+		
+		//delete-it will delete from index 0 and take 2places
+		sb1.delete(0, 2);
+		System.out.println(sb1);
+	}
+
+}
