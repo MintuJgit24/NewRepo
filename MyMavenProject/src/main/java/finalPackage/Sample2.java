c716b24e9a9147bd78a9e2a60cc86881071dc4c9
mintu <mintu@VJ-GALAXY-BOOK>
2025-01-14 11:32:46 +0530

    final keyword programs

---
+++ MyMavenProject/src/main/java/finalPackage/Sample2.java
@@ -0,0 +1,20 @@
+package finalPackage;
+//example for final keyword variable
+public class Sample2 extends Sample1 {
+ 
+	//final int a=10;
+    int b=20;
+	
+ public void display() {
+	 
+	 b=30;
+	 //a=18;
+ }
+ 
+ 
+ public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
